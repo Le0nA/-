d696153b54889f33c1f2be84350d83322374e0c5
Le0nA <43818663+Le0nA@users.noreply.github.com>
2019-03-03 22:03:20 +0300

    Update 3..cpp

--- дз 03.03.2019/3..cpp
+++ дз 03.03.2019/3..cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 struct Node {
 	char data;
-	Node* next;
+	Node* next = NULL;
 	Node* prev = NULL;
 };
 class char_deque {
@@ -9,37 +9,52 @@ private:
 	Node* head;
 	Node* tail;
 public:
-	char_deque(): head(NULL) {}
+	char_deque(): head(NULL), tail(NULL) {}
 	void push_front(char x) {
 		Node* temp = new Node;
-		temp->next = head;
-		head->prev = temp;
-		temp->data = x;
-		head = temp;
-	}
-	void push_back(char x) {
-		Node* temp = new Node;
-		temp->prev = head;
-		head->next = temp;
-		temp->data = x;
-		head = temp;
-	}
-	char pop_front() {
-		Node* temp = head;
-		while (temp->next) {
-			temp = temp->next;
-		}
-		char x = temp->data;
-		delete temp;
-		return x;
-	}
-	char pop_back() {
-		Node* temp = tail;
-		while (temp->prev) {
-			temp = temp->prev;
-		}
-		char x = temp->data;
-		delete temp;
-		return x;
-	}
+                temp ->prev = NULL;
+                temp ->data = x;
+        if(head != NULL){
+            head ->prev = temp;
+            temp ->next = head;
+            head = temp;
+        }
+        else{
+            temp ->next = NULL;
+            tail = temp;
+            head = tail;
+        }
+    }
+    void push_back(char x){
+        Node* temp = new Node;
+        temp ->next = NULL;
+        temp ->data = x;
+        if(tail != NULL){
+            tail ->next = temp;
+            temp ->prev = tail;
+            tail = temp;
+        }
+        else{
+            temp ->prev = NULL;
+            tail = temp;
+            head = tail;
+        }
+    }
+    void pop_fort() {
+    char x = head ->data;
+    cout<<x;
+    head = head ->next;
+    }
+    void pop_bck(){
+    char x = tail ->data;
+    cout<<x;
+    tail = tail ->prev;
+    }
+    ~char_deque(){
+    while(head != NULL){
+        Node* temp = head;
+        head = head ->next;
+        delete temp;
+        }
+    }
 };
